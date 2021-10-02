f88961273e50828bc4296e0e605ab91ecc197bf5
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2021-10-02 11:46:40 +0200

    Mixin now applies correctly in non-dev environment

--- src/main/java/sindarin/dontrunwithscissors/mixin/ShearsItemExtensions.java
+++ src/main/java/sindarin/dontrunwithscissors/mixin/ShearsItemExtensions.java
@@ -4,15 +4,21 @@ import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ShearsItem;
+import net.minecraft.item.*;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
 import sindarin.dontrunwithscissors.DontRunWithScissors;
 
 @Mixin(ShearsItem.class)
-public class ShearsItemExtensions {
+public abstract class ShearsItemExtensions extends Item {
+
+    public ShearsItemExtensions(Settings settings) {
+        super(settings);
+    }
+
+    @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (!world.isClient && selected && entity.isSprinting()) {
             entity.setSprinting(false);
